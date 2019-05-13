9592ca14e16d2436a91528777f7d9b0e4d76e4e5
Manash-witwicky <manashdas29@gmail.com>
2019-05-13 14:57:11 +0530

    Added json-server and Angular materila

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'emailApp';
+export class AppComponent implements OnInit {
+
+  private _url = 'http://localhost:3000/posts';
+
+  private posts: any = [];
+
+  constructor(private _http: HttpClient) { }
+
+  public getPost() {
+    // this._http.get(this._url)
+    //   .subscribe((post) => {
+    //     this.posts = post;
+    //     console.log(this.posts);
+    //   });
+    this.posts = this._http.get(this._url);
+    console.log(this.posts);
+
+  }
+
+  ngOnInit() {
+    this.getPost();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
+import { MatButtonModule } from '@angular/material';
+import { MatListModule } from '@angular/material/list';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatListModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
